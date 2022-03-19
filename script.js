ea8a54baa6de009011ceefe9d2570655cbec98d7
vhcamposq <viniciushcquadros@hotmail.com>
2022-03-18 21:31:42 -0300

    Adicionando mudança de lados

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function posicaoRandomica(){
 
     var mosquito = document.createElement('img') //cria o elemento da imagem
     mosquito.src = 'imagens/mosquito.png' // adiciona a imagem no elemento
-    mosquito.className = tamanhoAleatorio() // aplica o estilo css na imagem  ( codigo anterior mosquito.className = 'mosquito1')
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio() // aplica o estilo css na imagem  ( codigo anterior mosquito.className = 'mosquito1')
     mosquito.style.left = horizontal + 'px' // adiciona uma posição  hotizontal aleatória
     mosquito.style.top = vertical + 'px' // adiciona uma posição vertical aleatória
     mosquito.style.position = 'absolute' // coloca a imagem como absoluta
@@ -40,4 +40,11 @@ function tamanhoAleatorio(){
         return 'mosquito3'
     }
 }
-console.log(tamanhoAleatorio())
+function ladoAleatorio(){
+    var classe = Math.floor(Math.random() * 3)
+    if (classe == 0) {
+        return 'ladoA'
+    } else{
+        return 'ladoB'
+    }
+}
