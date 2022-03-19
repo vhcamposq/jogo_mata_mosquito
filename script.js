306792bc26b26cfc82eb630ade556d35fc196041
vhcamposq <viniciushcquadros@hotmail.com>
2022-03-18 21:24:26 -0300

    Adicionando Tamanhos aleatórios dos mosquitos

--- script.js
+++ script.js
@@ -13,11 +13,14 @@ function posicaoRandomica(){
     var horizontal = Math.floor(Math.random() * largura) - 90
     var vertical = Math.floor(Math.random() * altura) - 90
 
+    horizontal = horizontal < 0 ? 0 : horizontal // se posição x for menor que zero então recebe 0 (ele mesmo na posição original)
+    vertical = vertical < 0 ? 0 : vertical // se posição y for menor que zero entapo recebe 0 (ele mesmo na posição original)
+
     //criando elemento html
 
     var mosquito = document.createElement('img') //cria o elemento da imagem
     mosquito.src = 'imagens/mosquito.png' // adiciona a imagem no elemento
-    mosquito.className = 'mosquito1' // aplica o estilo css na imagem
+    mosquito.className = tamanhoAleatorio() // aplica o estilo css na imagem  ( codigo anterior mosquito.className = 'mosquito1')
     mosquito.style.left = horizontal + 'px' // adiciona uma posição  hotizontal aleatória
     mosquito.style.top = vertical + 'px' // adiciona uma posição vertical aleatória
     mosquito.style.position = 'absolute' // coloca a imagem como absoluta
@@ -26,3 +29,15 @@ function posicaoRandomica(){
 
     // depois adicionar <script> com a função no body
 }
+ 
+function tamanhoAleatorio(){
+    var classe = Math.floor(Math.random() * 3)
+    if (classe == 0) {
+        return 'mosquito1'
+    } else if (classe == 1) {
+        return 'mosquito2'
+    }else{
+        return 'mosquito3'
+    }
+}
+console.log(tamanhoAleatorio())
